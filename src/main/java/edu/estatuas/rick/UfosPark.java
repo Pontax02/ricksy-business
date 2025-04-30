51dc00e1f562302b21d571f25f35ebca185d3714
Pontax02 <ppontanilla07@gmail.com>
2025-04-30 11:24:17 +0200

    :sparkles: feat(Ufospark): Implementar el uso del hashmap y metodo dispatch para cargar a tarjeta

--- src/main/java/edu/estatuas/rick/UfosPark.java
+++ src/main/java/edu/estatuas/rick/UfosPark.java
@@ -1,26 +1,35 @@
 package edu.estatuas.rick;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UfosPark implements GuestDispatcher {
 
     private int fee;
-    private List<String> flota = new ArrayList<String>();
+    public Map<String,String> flota = new HashMap<String,String>();
 
     public UfosPark() {}
 
     public void add(String ufo){
-        this.flota.add(ufo);
+        this.flota.put(ufo,null);
     }
-
-
-
-
-
+    public void addClient(String ufo,CreditCard client){
+        this.flota.put(ufo,client.number());
+    }
+    public String getUfoOf(String ufo){
+        return this.flota.get(ufo);
+    }
 
     @Override
     public void dispatch(CreditCard card) {
+        if (card.credit() > 500){
+            card.setCredit(card.credit() - 500);
 
+        }
+        else {
+            card.setCredit(card.credit());
+        }
     }
 }
