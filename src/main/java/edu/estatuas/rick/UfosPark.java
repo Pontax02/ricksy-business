2c5d8c413bdd1e43c399a304dfc69f3aede3dee7
Pontax02 <ppontanilla07@gmail.com>
2025-05-04 14:48:51 +0200

    :rocket: fix(UfosPark): Arreglar metodo Dispatch
    Arreglar metodo Dispatch añadiendo los metodos getUfo para obtener un ufo libre , y el mtodo assign ufo para asignarle a un nuevo usuario su UFO

--- src/main/java/edu/estatuas/rick/UfosPark.java
+++ src/main/java/edu/estatuas/rick/UfosPark.java
@@ -13,19 +13,32 @@ public class UfosPark implements GuestDispatcher {
     public UfosPark() {}
 
     public void add(String ufo){
-        this.flota.put(ufo,null);
+        this.flota.put(null,ufo);
     }
-    public void addClient(String ufo,CreditCard client){
-        this.flota.put(ufo,client.number());
+    public void assignUfo(String user,String ufo){
+        this.flota.put(user,ufo);
     }
+
     public String getUfoOf(String ufo){
         return this.flota.get(ufo);
+    }
+    public String getUfo(){
+
+        String ufoSelected ="";
+        for (Map.Entry<String,String> entry : this.flota.entrySet()) {
+            if(entry.getKey()==null) {
+                ufoSelected = entry.getValue();
+            }
+        }
+        return ufoSelected;
     }
 
     @Override
     public void dispatch(CreditCard card) {
-        if (card.credit() > 500){
+
+        if (card.credit() > 500 && !flota.containsKey(card.number())) {
             card.setCredit(card.credit() - 500);
+            assignUfo(card.number(),getUfo());
 
         }
         else {
