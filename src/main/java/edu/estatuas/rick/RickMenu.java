e4ab137166d6af0456083c5a8b05e0f5cfa024c9
Pontax02 <ppontanilla07@gmail.com>
2025-05-05 15:07:33 +0200

    feat(RickMenu): Implementar el metodo de la interfaz y getters a la clase

--- src/main/java/edu/estatuas/rick/RickMenu.java
+++ src/main/java/edu/estatuas/rick/RickMenu.java
@@ -5,10 +5,29 @@ public class RickMenu implements GuestDispatcher{
         public int stock = 100;
         public double priceMenu = 10;
 
-
+        public RickMenu(int stock, double priceMenu) {
+                this.priceMenu = priceMenu;
+                this.stock = stock;
+        }
 
         @Override
         public void dispatch(CreditCard card) {
+                if(getStock() >= 1 && card.credit() > getPriceMenu()) {
+                        buyStock((short) 1);
+                        card.pay(getPriceMenu());
+                }
+        }
 
+        public double getPriceMenu() {
+                return this.priceMenu;
+        }
+
+        public void buyStock(short quantity) {
+                this.stock -= quantity;
+        }
+
+
+        public int getStock() {
+                return this.stock;
         }
 }
